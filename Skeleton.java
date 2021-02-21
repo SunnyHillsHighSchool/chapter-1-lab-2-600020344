affb7e7ca8739049d8214067c6e0094a34286717
600020344 <77069279+600020344@users.noreply.github.com>
2021-02-21 00:42:39 -0800

    Update Skeleton.java

--- Skeleton.java
+++ Skeleton.java
@@ -3,10 +3,44 @@ import static java.lang.System.*;
 public class Skeleton implements Monster
 {
  //add instance variables 
-
+private int size;
+private String name;
  //add a constructor
-
+public Skeleton(String n, int s)
+{
+  name = n;  
+  size = s;
+}
  //add code to implement the Monster interface
-
+ public int getHowBig()
+ {
+   return size;
+ }
+ public String getName()
+ {
+   return name;
+ }
+ public boolean isBigger(Monster other)
+ {
+   if (size > other.getHowBig())
+    return true;
+  return false;
+ }
+ public boolean isSmaller(Monster other)
+  {
+   if (size < other.getHowBig())
+    return true;
+  return false;
+ }
+ public boolean namesTheSame(Monster other)
+  {
+   if (name.equals(other.getName()))
+    return true;
+  return false;
+ }
  //add a toString
+ public String toString()
+ {
+   return name + " " + size;
+ }
 }
